55998c246eb3f2259c928cf91939f146b9a0017a
Jon05perei <162651961+Jon05perei@users.noreply.github.com>
2024-03-08 21:51:33 -0600

    menu

--- hola/Proyecto.py
+++ hola/Proyecto.py
@@ -1,3 +1,25 @@
 """
 hola
-"""
+"""#declaracion
+orden=0
+#datos
+print("menu:")
+print("1.casado")
+print("2.olla de carne")
+print("3.costilla a la diabla")
+pedidos=int(input("cuantas ordenes desea realizar:"))
+if pedidos==1:
+    orden=int(input("digite la opcion del menu que desea ordenar"))
+    print("orden recibida en la cocina")
+    print("su orden estara lista en 5 minutos")
+
+else:
+    if pedidos==2:
+        orden=int(input("digite la opcion del menu que desea ordenar"))
+        print("orden recibida en la cocina")
+        print("su orden estara lista en 5 minutos")
+    else:
+        if pedidos==3:
+            orden=int(input("digite la opcion del menu que desea ordenar"))
+            print("orden recibida en la cocina")
+            print("su orden estara lista en 5 minutos")
