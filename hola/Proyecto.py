19b2f9fe12bae0c4d5d1b2ca6c66c0a0643c04cb
Juan Pablo <162513075+Jchole905@users.noreply.github.com>
2024-03-15 18:31:50 -0600

    Proyecto.py
    Actualizar el proyecto

--- hola/Proyecto.py
+++ hola/Proyecto.py
@@ -1,5 +1,6 @@
 """
 hola
+<<<<<<< Updated upstream
 """
 #declaracion
 orden=0
@@ -24,3 +25,53 @@ else:
             orden=int(input("digite la opcion del menu que desea ordenar"))
             print("orden recibida en la cocina")
             print("su orden estara lista en 5 minutos")
+=======
+
+"""
+
+
+#Declaracion de variables
+descuento= 0
+num_Mesas= 0
+precio_Mesas= 0
+total_Cuenta= 0
+
+
+#Solicitud de datos
+num_Mesas=int(input("Indique la cantidad de mesas"))
+
+#Bucle para solicitar y acumular
+
+for i in range (num_Mesas):
+    precio_Mesas=int(input("Cuanto cuesta cada mesa" +str(i+1)+": "))
+
+#Condicion de precio
+
+
+if(precio_Mesas<=7500):
+    descuento=precio_Mesas*0.30
+    precio_Mesas=precio_Mesas-descuento
+    print("Esta reservacion tiene un descuento de  "+str(i+1)+": ",precio_Mesas)
+
+elif (precio_Mesas>11000):
+    precio_Mesas=precio_Mesas-descuento
+    print("Esta reservacion tiene un descuento de  "+str(i+1)+": ",precio_Mesas)
+
+
+
+total_Cuenta=total_Cuenta+precio_Mesas #Acumulador
+
+
+#Fuera del Bucle
+
+print("La factura en total  ", total_Cuenta)
+
+
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
