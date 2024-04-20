bec9c2992a8c9d21b0fba7479dcca250a1ce28c7
Jon05perei <162651961+Jon05perei@users.noreply.github.com>
2024-04-20 09:36:44 -0600

    login
    login

---
+++ Proyecto de programacion G3/Login proyecto.py
@@ -0,0 +1,30 @@
+#variables
+nombre=0
+cedula=0
+
+continuar= True
+while continuar= True:
+# Menú principal
+print("\n--- Menú ---")
+print("1. Reservacion de mesas")
+print("2. Gestion de pedidos")
+print("3. Facturacion")
+print("4. Salir")
+opc = input("Seleccione una opción: ")
+if valideVacios(opc):
+    
+        opc = int(opc)
+        if opc >= 1 and opc <= 3:
+            if opc == 1:
+                nombre = input("Ingrese su nombre: ")
+                cedula = input("Ingrese su cedula : ")
+
+            elif opc == 2:
+                #Gestion de pedidos
+            elif opc == 3:
+                #Facturacion
+            elif opc == 4:
+                print("Hasta Luego")
+       else:
+            print("Opción inválida. Intente nuevamente.")
+    
