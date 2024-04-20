f9f1f931db7444cce42706351ba011086e91085f
MrJAguilar <147429025+MrJAguilar@users.noreply.github.com>
2024-04-20 09:48:11 -0600

    correciones

--- Proyecto de programacion G3/ventas y facturación.py
+++ Proyecto de programacion G3/ventas y facturación.py
@@ -1,21 +1,19 @@
 """
 Ventas y Facturación
 """
-
 #Declaración de varibales
 total_venta=0
-
 #Menú de productos y precios
-productos= {
-  "1": {"nombre": "Casado", "precio": 10}
-  "2": {"nombre": "Olla de carne", "precio": 8}
+productos={
+  "1": {"nombre": "Casado", "precio": 10},
+  "2": {"nombre": "Olla de carne", "precio": 8},
   "3": {"nombre": "Costilla a la diabla", "precio": 12}
-
+}
 #Mostrar el menú de productos
 print("Menú de productos:")
 for clave in productos:
   productos=productos[clave]
-  print(clave+". " + producto["nombre"] + "- $" + str(producto["precio"]))
+  print(clave+". " + productos["nombre"] + "- $" + str(productos["precio"]))
 
 #Realizar ventas
   while opcion != "0":
@@ -24,7 +22,7 @@ for clave in productos:
       cantidad=int(input("Cantidad de " +productos[opcion]["nombre"] + " vendidos: "))
       total_venta+= productos[opcion]["precio"] * cantidad
       print("Venta registrada.")
-    elif opcion = "0":
+    elif opcion == 0:
       print("Opción inválida.")
 
 #Mostrar total de la venta y generar factura
